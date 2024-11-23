5a95fc3a8f4a565016ca61ef2fa5f670d6d6bf95
Wesley Hitson <wesleyhitson106@gmail.com>
2024-11-22 20:28:24 -0500

    working generate script

--- generate.py
+++ generate.py
@@ -1,9 +1,27 @@
 import sqlite3
+import json
+import csv
 
 def main():
-    con = sqlite3.connect("parks.db")
-    cur = con.cursor()
+    with open("parks.csv", "w", newline="", encoding="UTF-8") as csvfile:
+        writer = csv.writer(csvfile, dialect="excel")
 
-    cur.execute("CREATE TABLE if not exists parks(id primary key, name, year_founded, visited tinyint default 0)")
+        with open("parks.json", "r", encoding="UTF-8") as f:
+            data = json.load(f)
+
+            for p in data["data"]:
+                row = []
+                row.append(p["name"])
+                row.append(p["parkCode"])
+                row.append(p["designation"])
+                row.append(p["states"])
+                row.append(p["latitude"])
+                row.append(p["longitude"])
+                writer.writerow(row)
+
+    # con = sqlite3.connect("parks.db")
+    # cur = con.cursor()
+
+    # cur.execute("CREATE TABLE if not exists parks(id primary key, name, year_founded, visited tinyint default 0)")
 
 main()
